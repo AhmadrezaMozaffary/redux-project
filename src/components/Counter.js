ca453fff7bfbfd6b9503a2b0c163ee1c92471f85
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-07-05 23:47:38 +0430

    Show counter and other functionalities based on show state arrived from redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,21 +4,24 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counterVal = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incHandler = () => {
-    dispatch({ type: "increment" });
+    if (show) dispatch({ type: "increment" });
   };
 
   const decHandler = () => {
-    dispatch({ type: "decrement" });
+    if (show) dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counterVal}</div>
+      {show && <div className={classes.value}>{counterVal}</div>}
       <div>
         <button onClick={incHandler}>+1</button>
         <button onClick={decHandler}>-1</button>
